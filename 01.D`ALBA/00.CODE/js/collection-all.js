7065aeb27c4b584b7375520a348088b0309bc923
Min Kyung <01aldrn10@gmail.com>
2024-01-17 04:17:05 +0900

    s

---
+++ 01.D`ALBA/00.CODE/js/collection-all.js
@@ -0,0 +1,29 @@
+import svgData from '../assets/data/svgData.js'
+
+window.addEventListener('DOMContentLoaded',function(){
+  // svg
+  const arrowLeft = document.querySelector('.collection-content__arrow .arrow-left')
+  const arrowRight = document.querySelector('.collection-content__arrow .arrow-right')
+  arrowLeft.innerHTML = svgData.arrowLeft_w
+  arrowRight.innerHTML = svgData.arrowRight_w
+
+  // collection-bg__slide
+  const collectionSwiper1 = new Swiper('.collection-bg__swiper',{
+    direction: 'horizontal',
+    loop: true,
+    effect:'fade',
+    touchRatio: 0, // 드래그 막기
+  })
+
+  // collection-content__slide
+  const collectionSwiper2 = new Swiper('.collection-content__swiper',{
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+  })
+
+
+})
+
+
+
