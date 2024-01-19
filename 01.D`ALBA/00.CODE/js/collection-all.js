ea96dba8d617ffeba5c2b0e7672a5f5af1314472
Min Kyung <01aldrn10@gmail.com>
2024-01-20 05:36:16 +0900

    s

--- 01.D`ALBA/00.CODE/js/collection-all.js
+++ 01.D`ALBA/00.CODE/js/collection-all.js
@@ -2,28 +2,39 @@ import svgData from '../assets/data/svgData.js'
 
 window.addEventListener('DOMContentLoaded',function(){
   // svg
-  const arrowLeft = document.querySelector('.collection-content__arrow .arrow-left')
-  const arrowRight = document.querySelector('.collection-content__arrow .arrow-right')
+  const arrowLeft = document.querySelector('.arrow-left')
+  const arrowRight = document.querySelector('.arrow-right')
   arrowLeft.innerHTML = svgData.arrowLeft_w
   arrowRight.innerHTML = svgData.arrowRight_w
 
   // collection-bg__slide
   const collectionSwiper1 = new Swiper('.collection-bg__swiper',{
+    centeredSlides: true,
     loop: true,
-    effect:'fade',
-    touchRatio: 0, // 드래그 막기
-  })
+    loopedSlides: 4,
+    // touchRatio: 0,
+    loopAdditionalSlides :1,
 
+    allowTouchMove:false,
+    effect:'fade',
+    // navigation: {
+    //   nextEl: '.arrow-right',
+    //   prevEl: '.arrow-left',
+    // },
+  })
+  
   // collection-content__slide
   const collectionSwiper2 = new Swiper('.collection-content__swiper',{
-    loop: true,
-    slidesPerView: 2,
     centeredSlides: true,
-    // loopedSlides: 2,
+    slideToClickedSlide: true,
+    loop: true,
+    loopAdditionalSlides :1,
+    loopedSlides: 4,
     navigation: {
       nextEl: '.arrow-right',
       prevEl: '.arrow-left',
     },
+    
   })
   
   collectionSwiper1.controller.control = collectionSwiper2
