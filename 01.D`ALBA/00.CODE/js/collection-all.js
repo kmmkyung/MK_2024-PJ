591df2524c8ee6f798d48750de2fe64bfd29ae52
Min Kyung <01aldrn10@gmail.com>
2024-01-20 01:53:00 +0900

    s

--- 01.D`ALBA/00.CODE/js/collection-all.js
+++ 01.D`ALBA/00.CODE/js/collection-all.js
@@ -1,38 +1,35 @@
-// import svgData from '../assets/data/svgData.js'
+import svgData from '../assets/data/svgData.js'
 
-// window.addEventListener('DOMContentLoaded',function(){
-//   // svg
-//   const arrowLeft = document.querySelector('.collection-content__arrow .arrow-left')
-//   const arrowRight = document.querySelector('.collection-content__arrow .arrow-right')
-//   arrowLeft.innerHTML = svgData.arrowLeft_w
-//   arrowRight.innerHTML = svgData.arrowRight_w
+window.addEventListener('DOMContentLoaded',function(){
+  // svg
+  const arrowLeft = document.querySelector('.collection-content__arrow .arrow-left')
+  const arrowRight = document.querySelector('.collection-content__arrow .arrow-right')
+  arrowLeft.innerHTML = svgData.arrowLeft_w
+  arrowRight.innerHTML = svgData.arrowRight_w
 
-//   // collection-bg__slide
-//   const collectionSwiper1 = new Swiper('.collection-bg__swiper',{
-//     direction: 'horizontal',
-//     loop: true,
-//     effect:'fade',
-//     touchRatio: 0, // 드래그 막기
-//     loopedSlides: 2,
-//   })
+  // collection-bg__slide
+  const collectionSwiper1 = new Swiper('.collection-bg__swiper',{
+    loop: true,
+    effect:'fade',
+    touchRatio: 0, // 드래그 막기
+  })
 
-//   // collection-content__slide
-//   const collectionSwiper2 = new Swiper('.collection-content__swiper',{
-//     direction: 'horizontal',
-//     loop: true,
-//     // slidesPerView: 3,
-//     centeredSlides: true,
-//     loopedSlides: 2,
-//     // navigation: {
-//     //   nextEl: '.collection-content__arrow .arrow-right',
-//     //   prevEl: '.collection-content__arrow .arrow-left',
-//     // },
-//   })
+  // collection-content__slide
+  const collectionSwiper2 = new Swiper('.collection-content__swiper',{
+    loop: true,
+    slidesPerView: 2,
+    centeredSlides: true,
+    // loopedSlides: 2,
+    navigation: {
+      nextEl: '.arrow-right',
+      prevEl: '.arrow-left',
+    },
+  })
   
-//   // collectionSwiper2.controller.control = collectionSwiper1
-//   // collectionSwiper1.controller.control = collectionSwiper2
+  collectionSwiper1.controller.control = collectionSwiper2
+  collectionSwiper2.controller.control = collectionSwiper1
 
-// })
+})
 
 
 
