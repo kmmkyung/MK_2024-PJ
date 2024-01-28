e2c505ea98fc95cab4882391c73b6cd6a4eb77d3
Min Kyung <01aldrn10@gmail.com>
2024-01-29 00:25:14 +0900

    Update story-verification.js

--- 01.D`ALBA/00.CODE/js/story-verification.js
+++ 01.D`ALBA/00.CODE/js/story-verification.js
@@ -2,11 +2,9 @@ window.addEventListener('DOMContentLoaded',function(){
   const content = document.querySelectorAll('.content');
   const contentMenu = document.querySelectorAll('.content-menu li');
 
-  contentMenu.forEach(function(ele,idx){
-    ele.addEventListener('click',function(){
-      let activeEle = contentMenu
-      ele.classList.add('active')
-
-    })
+  contentMenu.addEventListener('click',function(){
+    console.log('adfad');
+    
   })
+
 })
