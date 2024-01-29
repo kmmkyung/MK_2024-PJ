142e4a6bf0003ce4da226342cee7b79b2d6bfd13
Min Kyung <01aldrn10@gmail.com>
2024-01-30 04:58:31 +0900

    s

--- 01.D`ALBA/00.CODE/js/story-verification.js
+++ 01.D`ALBA/00.CODE/js/story-verification.js
@@ -2,9 +2,16 @@ window.addEventListener('DOMContentLoaded',function(){
   const content = document.querySelectorAll('.content');
   const contentMenu = document.querySelectorAll('.content-menu li');
 
-  contentMenu.addEventListener('click',function(){
-    console.log('adfad');
-    
+  contentMenu.forEach(function(ele,idx){
+    ele.addEventListener('click',()=>{
+      for(let i=0; i<contentMenu.length; i++){
+        contentMenu[i].classList.remove('active')
+        content[i].classList.remove('active')
+      }
+      ele.classList.add('active')
+      content[idx].classList.add('active')
+    })
   })
 
+  
 })
