d83bd72407577af371a729b49089b1be88b143ff
Min Kyung <01aldrn10@gmail.com>
2024-02-12 02:25:01 +0900

    s

--- 01.D`ALBA/00.CODE/js/collection-theme.js
+++ 01.D`ALBA/00.CODE/js/collection-theme.js
@@ -1,17 +1,37 @@
-const swiper1 = new Swiper('.section-2__swiper',{
-  direction: 'horizontal',
-  loop: true,
-  // autoplay: {
-  //   delay: 3000,
-  // },
-  effect:'fade',
-  fadeEffect: { crossFade: true },
-  navigation:{
-    nextEl: '.section-2__navigation-button-next',
-    prevEl: '.section-2__navigation-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
+window.addEventListener('DOMContentLoaded',function(){
+
+  
+  const swiper1 = new Swiper('.section-2__swiper',{
+    direction: 'horizontal',
+    loop: true,
+    // autoplay: {
+      //   delay: 3000,
+      // },
+      effect:'fade',
+      fadeEffect: { crossFade: true },
+      navigation:{
+        nextEl: '.section-2__navigation-button-next',
+        prevEl: '.section-2__navigation-button-prev',
+      },
+      pagination: {
+        el: '.section-2__swiper .swiper-pagination',
+        clickable: true
+      },
+    })
+
+    const swiper2 = new Swiper('.section-5__swiper',{
+    direction: 'horizontal',
+    loop: true,
+    // autoplay: {
+      //   delay: 3000,
+      // },
+      effect:'fade',
+      fadeEffect: { crossFade: true },
+      navigation:{
+        nextEl: '.section-5__navigation-button-next',
+        prevEl: '.section-5__navigation-button-prev',
+      }
+    })
+
+
 })
