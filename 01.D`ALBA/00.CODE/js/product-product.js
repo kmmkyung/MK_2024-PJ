72e7d1c4d84d88c15777700e69f5c2483bd858f9
Min Kyung <01aldrn10@gmail.com>
2024-02-19 20:41:00 +0900

    s

--- 01.D`ALBA/00.CODE/js/product-product.js
+++ 01.D`ALBA/00.CODE/js/product-product.js
@@ -1,5 +1,5 @@
 import svgData from '../assets/data/svgData.js'
-import gnbData from '../assets/data/gnbData.js';
+import {gnbData} from '../assets/data/gnbData.js';
 
 window.addEventListener('DOMContentLoaded',function(){
   // topMenu
@@ -8,6 +8,19 @@ window.addEventListener('DOMContentLoaded',function(){
   filterButton.innerHTML = svgData.icon_filter
 
   //// menu-1__tapMenu
+  const tapMenu = document.querySelector('.menu-1__tapMenu')
+  let tapMenuLi = ``;
+  for (let menuList of gnbData.Product){
+    tapMenuLi +=`<li class="tapMenuList"><a href="#">${menuList}</a></li>` 
+  }
+  tapMenu.innerHTML = tapMenuLi
   
-
+  const menu1Title = document.querySelector('.menu-1 h2')
+  menu1Title.addEventListener('click',function(){
+    menu1Title.classList.toggle('active')
+    tapMenu.classList.toggle('active')
+    if(tapMenu.classList.contains('active')){
+      tapMenu.style.height = auto
+    }
+  })
 })
