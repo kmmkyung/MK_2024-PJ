a3e3fc367a6288c7ecb60dcbb4eddd4d600b166b
Min Kyung <01aldrn10@gmail.com>
2024-03-05 00:53:53 +0900

    s

---
+++ 01.D`ALBA/00.CODE/assets/data/museCampaignData.js
@@ -0,0 +1,13 @@
+const museCampaignData = {
+  muse1:{
+    "section1-video":"muse1-vid.mp4",
+    "section1-h6":"2019 fw",
+    "section1-h1":"세럼을 스프레이 하다",
+    "section1-p":"늘 도전적인 그녀처럼<br>바쁜 일상 속 프리미엄 스킨케어를 놓칠 수 없는<br>당신을 위한 달바의 도전, 미스트 세럼입니다.",
+    "section2-bg":"muse1-2.jpg",
+    "section2-h2":"달바 X 한혜진 1st 캠페인 화보",
+    "section2-p":"달바와 한혜진이 함께한 첫 번째 캠페인<br>늘 열정적인 뮤즈 한혜진으로부터 영감을 받은<br>달바의 첫 번째 캠페인입니다.",
+    "section3-bg":"muse1-3.jpg",
+    "section-3-item":['muse1-3_1.jpg','muse1-3_2.jpg','muse1-3_3.jpg']
+  },
+}
