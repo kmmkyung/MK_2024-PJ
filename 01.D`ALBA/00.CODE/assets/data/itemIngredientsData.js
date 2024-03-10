9c9cda4bad1a51d647b4363bb297cb3ef00ac123
Min Kyung <01aldrn10@gmail.com>
2024-03-10 22:39:30 +0900

    Update itemIngredientsData.js

--- 01.D`ALBA/00.CODE/assets/data/itemIngredientsData.js
+++ 01.D`ALBA/00.CODE/assets/data/itemIngredientsData.js
@@ -10,8 +10,8 @@ const itemIngredientsData = {
     "ingredients-title":"IndiMultiEx WH-9 Complex (N)",
     "ingredients-text":"사우수레아인볼루크라타추출물, 인삼추출물, 연꽃꽃추출물, 뽕나무껍질추출물, 마돈나백합꽃추출물, 에델바이스추출물, 약모밀추출물, 프리지어추출물, 데이지꽃추출물",
     "ingredients-caption":"피부에 수분을 공급하여 촉촉하고 보습감 있는 피부로 가꾸어 주는 데 도움을 줍니다.",
-    "ingredients-bg__pc":"indiMultiEx.jpg",
-    "ingredients-bg__mobile":"mobile_indiMultiEx.jpg"
+    "ingredients-bg__pc":"IndiMultiEx.jpg",
+    "ingredients-bg__mobile":"mobile_IndiMultiEx.jpg"
   },
   "d'Alba-8 Blending Oil":{
     "ingredients-title":"d'Alba-8 Blending Oil",
