2a390359259cea291e4e1ae5575b84f552b46f84
Min Kyung <01aldrn10@gmail.com>
2024-04-15 00:41:49 +0900

    s

---
+++ 02.EATH-Library/EATH-Library/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout></NuxtLayout>
+</template>
+<script></script>
+<style lang="scss">
+  @use './assets/scss/main.scss'
+</style>
---
+++ 02.EATH-Library/EATH-Library/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <aside class="top-notice">
+      <NuxtLink class="top-notice-text" to="/">회원가입시 5% 할인쿠폰과 무료배송 혜택을 드립니다.</NuxtLink></aside>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/scss/abstracts';
+
+.top-notice{
+  width: 100%;
+  height: 40px;
+  background-color: #000;
+  color: #fff;
+  text-align: center;
+  
+  .top-notice-text{
+    font-size: abstracts.$font-size-s;
+    height: 100%;
+    line-height: 15px;
+  }
+}
+</style>
