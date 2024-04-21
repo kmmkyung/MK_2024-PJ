a72f93f280a91d0cf376aca0ec1ea6613d82ca34
Min Kyung <01aldrn10@gmail.com>
2024-04-21 12:03:47 +0900

    s

--- 02.EATH-Library/EATH-Library/pages/index.vue
+++ 02.EATH-Library/EATH-Library/pages/index.vue
@@ -2,20 +2,24 @@
   <div>
     <aside class="top-notice">
       <div class="top-notice__wrap">
-        <p>
-          <span class="text-brand">EATH LIBRARY</span>
-          <span>회원가입시 5% 할인쿠폰과 무료배송 혜택을 드립니다.</span>
-          <span class="text-brand">EATH LIBRARY</span>
-          <span>Evolutionary, Achievement from Traditional Heritage</span>
-          <span class="text-brand">EATH LIBRARY</span>
-          <span>당신의 손을 위한 가장 자연스러운 선택.</span>
-          <span class="text-brand">EATH LIBRARY</span>
-          <span>회원가입시 5% 할인쿠폰과 무료배송 혜택을 드립니다.</span>
-          <span class="text-brand">EATH LIBRARY</span>
-          <span>Evolutionary, Achievement from Traditional Heritage</span>
-          <span class="text-brand">EATH LIBRARY</span>
-          <span>당신의 손을 위한 가장 자연스러운 선택.</span>
-        </p>
+        <div class="text-wrap">
+          <p>
+            <span class="text-brand">EATH LIBRARY</span>
+            <span>회원가입시 5% 할인쿠폰과 무료배송 혜택을 드립니다.</span>
+            <span class="text-brand">EATH LIBRARY</span>
+            <span>Evolutionary, Achievement from Traditional Heritage</span>
+            <span class="text-brand">EATH LIBRARY</span>
+            <span>당신의 손을 위한 가장 자연스러운 선택.</span>
+          </p>
+          <p>
+            <span class="text-brand">EATH LIBRARY</span>
+            <span>회원가입시 5% 할인쿠폰과 무료배송 혜택을 드립니다.</span>
+            <span class="text-brand">EATH LIBRARY</span>
+            <span>Evolutionary, Achievement from Traditional Heritage</span>
+            <span class="text-brand">EATH LIBRARY</span>
+            <span>당신의 손을 위한 가장 자연스러운 선택.</span>
+          </p>
+        </div>
       </div>
     </aside>
   </div>
@@ -39,42 +43,74 @@
   
   .top-notice__wrap{
     // width: calc(100% - 50px);
-    // margin: 0 25px;
+    margin: 0 25px;
     overflow: hidden;
-    white-space: nowrap;
-        
-    p{
-      white-space: nowrap;
-      animation: topText 5s linear infinite;
+    position: relative;
+    
+    &::before{
+      content: '';
+      height: 40px;
+      width: 10px;
+      background: linear-gradient(90deg, rgb(0, 0, 0),rgba(0, 0, 0, 0));
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 1;
+    }
 
+    &::after{
+      content: '';
+      height: 40px;
+      width: 10px;
+      background: linear-gradient(-90deg, rgb(0, 0, 0),rgba(0, 0, 0, 0));
+      position: absolute;
+      top: 0;
+      right: 0;
+      z-index: 1;
+    }
+
+    .text-wrap{
+      white-space: nowrap;
+      display: flex;
+      flex-wrap: nowrap;
+      gap: 10vw;
+      width: max-content;
+      animation: topText 20s linear infinite;
+      
       @keyframes topText {
-        100% { transform: translateX(calc(-100%));
+        100% {
+          transform: translateX(calc(-50% + -5vw));
         }
       }
-      
+
       &:hover{
         animation-play-state: paused;
       }
-      
-      span:not(:last-child){
-        margin-right: 10vw;
-      }
-      
-      span{
-        display: inline-block;
-        text-align: left;
-        line-height: 40px;
-        font-size: abstracts.$font-size-s;
-        
-        &:hover{
-          text-decoration: underline;
+
+        p{
+          width: 100%;
+          white-space: nowrap;
+          
+          span{
+            display: inline-block;
+            text-align: left;
+            line-height: 40px;
+            font-size: abstracts.$font-size-s;
+            
+            &:hover{
+              text-decoration: underline;
+            }
+          }
+
+          span:not(:last-child){
+            margin-right: 10vw;
+          }
+
+          .text-brand{
+            font-weight: bold;
+          }
         }
-      }
-      
-      .text-brand{
-        font-weight: bold;
       }
     }
   }
-}
 </style>
