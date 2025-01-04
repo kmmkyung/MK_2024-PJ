d912704741ec1f785d4e63d1fa094b07ed162dbc
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2025-01-04 17:43:40 +0900

    s

--- 02.disney-plus/src/components/Banner.js
+++ 02.disney-plus/src/components/Banner.js
@@ -2,12 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axiosInstance from '../api/axios'; // api axios instance
 import requests from '../api/request'; // api request
 import '../css/Banner.css';
-import styled from'styled-components';
 
 function Banner() {
   // state
   let [ movie, setMovie ] = useState({});
-  let [ isClicked, setIsClicked ] = useState(false);
 
   // use effect
   useEffect(()=>{
@@ -30,74 +28,20 @@ function Banner() {
   // css
   const cssBanner = {
     backgroundImage : `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
-    backgroundPosition : 'top center center',
+    backgroundPosition : 'center',
     backgroundSize : 'cover',
   }
 
   // render 
-  // video play button click event
-  if(isClicked){
     return (
-      <>
-        <Container>
-          <HomeContainer>
-            <Iframe
-            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&mute=1&loop=1&playlist=${movie.videos.results[0].key}`}
-            width='640' height='360' frameBorder='0' allow='autoplay fullscreen'
-            ></Iframe>
-            <button className='banner__button close' onClick={()=>{setIsClicked(false)}}>X</button>
-          </HomeContainer>
-        </Container>
-      </>
-    )
-  }
-  else{
-    return (
-      <header className='banner' style={cssBanner}>
+      <div className='banner' style={cssBanner}>
         <div className='banner__contents'>
           <h1 className='banner__title'>{movie.title || movie.name || movie.original_name}</h1>
-          <div className='banner__buttons'>
-            {movie?.videos?.results[0]?.key && <button className='banner__button play' onClick={()=>{setIsClicked(true)}}>Play</button>}
-          </div>
           <p className='banner__description'>{movie.overview}</p>
         </div>
-        <div className='banner--fadeBottom'></div>
-      </header>
+      </div>
     )
   }
-}
+
 
 export default Banner;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  height: 80vh;
-`
-
-const HomeContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  position: relative;
-
-
-  &::after{
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-`
-
-const Iframe = styled.iframe`
-  width: 100%;
-  height: 100%;
-  opacity: 0.65;
-  border: none;
-
-`
