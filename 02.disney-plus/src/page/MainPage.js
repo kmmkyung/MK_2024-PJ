0fd8e15c392fb99b35da0cd1a18ef9d959be0c7f
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2025-01-09 22:00:24 +0900

    Update MainPage.js

--- 02.disney-plus/src/page/MainPage.js
+++ 02.disney-plus/src/page/MainPage.js
@@ -3,24 +3,23 @@ import '../css/App.css';
 import styled from'styled-components';
 import requests from '../api/request';
 
-
 import Banner from '../components/Banner';
 import Category from '../components/Category';
 import Row from '../components/Row';
 
 function MainPage(){
-  return(
-  <Main>
-    <Banner></Banner>
-    <Container>
-      <Category></Category>
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}></Row>
-      <Row title="Top Rated" id="TR" mediaType='movie' fetchUrl={requests.fetchTopRated}></Row>
-      <Row title="Now Movies" id="TM" mediaType='movie' fetchUrl={requests.fetchNowPlaying}></Row>
-      <Row title="Action Movies" id="AM" mediaType='movie' fetchUrl={requests.fetchActionMovies}></Row>
-      <Row title="Comedy Movies" id="CM" mediaType='movie' fetchUrl={requests.fetchComedyMovies}></Row>
-    </Container>
-  </Main>
+    return(
+      <Main>
+      <Banner></Banner>
+      <Container>
+        <Category></Category>
+        <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}></Row>
+        <Row title="Top Rated" id="TR" mediaType='movie' fetchUrl={requests.fetchTopRated}></Row>
+        <Row title="Now Movies" id="TM" mediaType='movie' fetchUrl={requests.fetchNowPlaying}></Row>
+        <Row title="Action Movies" id="AM" mediaType='movie' fetchUrl={requests.fetchActionMovies}></Row>
+        <Row title="Comedy Movies" id="CM" mediaType='movie' fetchUrl={requests.fetchComedyMovies}></Row>
+      </Container>
+    </Main>
   )
 }
 
