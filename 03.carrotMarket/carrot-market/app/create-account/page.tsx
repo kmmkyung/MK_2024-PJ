8e63760446cf468fd1ed9d27f9ddc2040131d1c8
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2025-02-16 20:35:33 +0900

    s

--- 03.carrotMarket/carrot-market/app/create-account/page.tsx
+++ 03.carrotMarket/carrot-market/app/create-account/page.tsx
@@ -1,15 +1,21 @@
 import FormButton from "@/components/FormButton";
 import FormInput from "@/components/FormInput";
 import SocialLogin from "@/components/SocialLogin";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 
 export default function CreateAccount(){
   return (
   <section className="flex flex-col gap-10 p-10">
-    <div className="flex flex-col gap-2 *:font-medium">
-      <h2 className="text-2xl">안녕하세요!</h2>
-      <h2 className="text-xl">Fill in the form below to join!</h2>
-    </div>
+    <div className="flex items-center justify-between">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h2 className="text-2xl">안녕하세요!</h2>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
+      </div>
+      <Link href='/' className="text-white">
+        <HomeIcon className="size-6 hover:text-primary transition-colors"/>
+      </Link>    </div>
     <form className="flex flex-col gap-3">
       <FormInput type="text" placeholder="User Name" required errors={[]}/>
       <FormInput type="email" placeholder="Email" required errors={[]}/>
--- 03.carrotMarket/carrot-market/app/login/page.tsx
+++ 03.carrotMarket/carrot-market/app/login/page.tsx
@@ -1,13 +1,20 @@
 import FormButton from "@/components/FormButton";
 import FormInput from "@/components/FormInput";
 import SocialLogin from "@/components/SocialLogin";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 export default function Login(){
   return (
   <section className="flex flex-col gap-10 p-10">
-    <div className="flex flex-col gap-2 *:font-medium">
-      <h2 className="text-2xl">안녕하세요!</h2>
-      <h2 className="text-xl">Login with email and password!</h2>
+    <div className="flex items-center justify-between">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h2 className="text-2xl">안녕하세요!</h2>
+        <h2 className="text-xl">Login with email and password!</h2>
+      </div>
+      <Link href='/' className="text-white">
+        <HomeIcon className="size-6 hover:text-primary transition-colors"/>
+      </Link>
     </div>
     <form className="flex flex-col gap-3">
       <FormInput type="email" placeholder="Email" required errors={[]}/>
