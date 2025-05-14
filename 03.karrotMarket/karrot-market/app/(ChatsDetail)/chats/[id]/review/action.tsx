47ed1eba85d9afd55440ce217973b41d5fd9952c
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2025-05-14 20:45:39 +0900

    s

--- 03.karrotMarket/karrot-market/app/(ChatsDetail)/chats/[id]/review/action.tsx
+++ 03.karrotMarket/karrot-market/app/(ChatsDetail)/chats/[id]/review/action.tsx
@@ -3,12 +3,9 @@
 import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { z } from "zod";
-import { getRoom } from "../action";
 
 const reviewSchema = z.string({required_error: "후기는 필수입니다"})
 .trim().min(5,"5자 이상 입력해주세요")
-
-const room = await getRoom(id)
 
 export async function saveReview(_: unknown, formData: FormData){
   const data = formData.get('review')
@@ -21,13 +18,13 @@ export async function saveReview(_: unknown, formData: FormData){
     if(session.id){
       const review = await db.review.create({
         data: {
-          review: result.data,
-          userId: session.id,
+          // review: result.data,
+          // userId: session.id,
           // users:,
           // productId: 
         }
       })
-      revalidateTag(`product-${formData.get('productId')}`)
+      // revalidateTag(`product-${formData.get('productId')}`)
       return { review }
     }
     else {
