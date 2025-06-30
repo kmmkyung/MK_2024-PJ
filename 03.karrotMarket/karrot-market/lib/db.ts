b99790efe822091831d121fcf419ce8f96f3492d
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2025-06-30 17:39:59 +0900

    s

--- 03.karrotMarket/karrot-market/lib/db.ts
+++ 03.karrotMarket/karrot-market/lib/db.ts
@@ -4,8 +4,13 @@ const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClient()
-
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL_Prisma, // <- 새 환경변수 사용
+    },
+  },
+})
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db
 }
