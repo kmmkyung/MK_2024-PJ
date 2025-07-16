28b4264a90a8f9d50f2431c55b51392ab58b5d3d
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2025-07-16 17:12:34 +0900

    s

--- 03.karrotMarket/karrot-market/app/(NoLogin)/login/action.ts
+++ 03.karrotMarket/karrot-market/app/(NoLogin)/login/action.ts
@@ -5,6 +5,7 @@ import db from "@/lib/db";
 import { z } from "zod";
 import bcrypt from "bcrypt"
 import userLogin from "@/lib/userLogin";
+import { redirect } from "next/navigation";
 
 async function checkEmail(email:string){
   const user = await db.user.findUnique({
--- 03.karrotMarket/karrot-market/lib/userLogin.ts
+++ 03.karrotMarket/karrot-market/lib/userLogin.ts
@@ -1,9 +1,11 @@
 import { redirect } from "next/navigation";
 import getSession from "./session"
+import { revalidatePath } from "next/cache";
 
 export default async function userLogin(id:number) {
   const session = await getSession();
   session.id = id;
   await session.save();
+  revalidatePath('/')
   return redirect('/profile');
 }
