be0b5b47fa574e59095aed2fef673eb5f7f41e7f
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2025-07-16 23:28:25 +0900

    favicon

--- 03.karrotMarket/karrot-market/app/layout.tsx
+++ 03.karrotMarket/karrot-market/app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
     template:"%s | 🥕Carrot",
     default: "🥕Carrot Market"
   },
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥕</text></svg>",
+  },
   description: "Sell and Buy all the things",
 };
 
